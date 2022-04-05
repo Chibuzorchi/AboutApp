193e3e633147828f0486aa105435ddef68ba9089
Chibuzorchi <chinonsochibuzor2277@gmail.com>
2022-04-05 13:36:52 +0100

    Aboutme app

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AboutApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Chinonso, I am in Backend Track of Wella Schools");
+            Console.WriteLine("I'm learning Backend because of my curious nature. I like to know how things work");
+        }
+    }
+}
